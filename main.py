4a13ab76b71fd603e45bfe8923e8107cf2b29cc3
Rivendell96 <dima.tyrin@gmail.com>
2025-01-26 21:35:19 +0200

    add count characters

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ def main():
 
     words = file_content.split()
     count_words = len(words)
+
+    character_dict = {}
+    for word in file_content:
+        for character in word.lower():
+            if character in character_dict:
+                character_dict[character] += 1
+            else:
+                character_dict[character] = 1
+    
+    print(character_dict)
     print(file_content)
     print(count_words)
 
