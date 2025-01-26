4def5773d6d6ab50315630569a6c0d3762b57a17
Rivendell96 <dima.tyrin@gmail.com>
2025-01-26 21:53:17 +0200

    modify the main

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_content = f.read()
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
+    count_words = get_num_words(book_text)
+    character_dict = get_chars_dict(book_text)
+    
+    print(character_dict)
+    print(book_text)
+    print(count_words)
 
-    words = file_content.split()
-    count_words = len(words)
 
+def get_chars_dict(text):
     character_dict = {}
-    for word in file_content:
+    for word in text:
         for character in word.lower():
             if character in character_dict:
                 character_dict[character] += 1
             else:
                 character_dict[character] = 1
-    
-    print(character_dict)
-    print(file_content)
-    print(count_words)
+
+    return character_dict
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_book_text(path):
+        with open(path) as f:
+            return f.read()
 
 main()
